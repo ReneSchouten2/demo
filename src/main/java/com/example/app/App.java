421503107e7ecaaf7b29d13ce709badf10b9344a
Joshua Lambert <joshua@gitlab.com>
2017-07-07 15:25:38 +0000

    Update App.java

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -2,10 +2,25 @@ package com.example.app;
 
 import com.example.dep.Dep;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( Dep.hello( "GitLab" ) );
+import java.io.*;
+import javax.xml.ws.*;
+import javax.xml.ws.http.*;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+
+@WebServiceProvider
+@ServiceMode(value = Service.Mode.PAYLOAD)
+public class Server implements Provider<Source> {
+
+    public Source invoke(Source request) {
+        return  new StreamSource(new StringReader(Dep.hello("GitLab"));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String address = "http://0.0.0.0:80/";
+        Endpoint.create(HTTPBinding.HTTP_BINDING, new Server()).publish(address);
+
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
