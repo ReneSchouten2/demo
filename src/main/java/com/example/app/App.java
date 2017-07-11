60f3888ca3dd0ca33c4c5c0e9c76e61300167877
Josh Lambert <gitlabdemouser@gmail.com>
2017-07-11 18:34:06 +0000

    switch to spark

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -2,25 +2,10 @@ package com.example.app;
 
 import com.example.dep.Dep;
 
-import java.io.*;
-import javax.xml.ws.*;
-import javax.xml.ws.http.*;
-import javax.xml.transform.*;
-import javax.xml.transform.stream.*;
+import static spark.Spark.*;
 
-@WebServiceProvider
-@ServiceMode(value = Service.Mode.PAYLOAD)
-public class App implements Provider<Source> {
-
-    public Source invoke(Source request) {
-        return  new StreamSource(new StringReader(Dep.hello("GitLab")));
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-
-        String address = "http://0.0.0.0:5000/";
-        Endpoint.create(HTTPBinding.HTTP_BINDING, new App()).publish(address);
-
-        Thread.sleep(Long.MAX_VALUE);
+public class GreetUser {
+    public static void main(String[] args) {
+        get("/", (req,res) -> Dep.hello("GitLab!") );
     }
 }
